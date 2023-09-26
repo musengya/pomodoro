e5aec3e8b597686f91cd039b77fa843ab9e28fd6
Irene Mutua <mutuamirene@gmail.com>
2023-09-26 07:59:45 +0300

    Add tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,2 +1,11 @@
-import React from 'react';
+import { describe, test, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
 import App from './App';
+
+describe('App', () => {
+  test('it displays correct heading', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { name: /pomodoro/i })).toBeInTheDocument();
+  });
+});
