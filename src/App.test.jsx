e930968c87c4f7842130d1192a08a48aeca78c76
Irene Mutua <mutuamirene@gmail.com>
2023-09-26 08:50:05 +0300

    Add tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,4 +8,9 @@ describe('App', () => {
     render(<App />);
     expect(screen.getByRole('heading', { name: /pomodoro/i })).toBeInTheDocument();
   });
+  test('it displays buttons', () => {
+    render(<App />);
+    expect(screen.getAllByRole('button', { name: /start/i })).toBeDefined();
+    expect(screen.getAllByRole('button', { name: /reset/i })).toBeDefined();
+  });
 });
