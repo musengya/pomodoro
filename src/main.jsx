9d1e657875424b447c2af911455802d55f6c563f
Irene Mutua <mutuamirene@gmail.com>
2023-10-16 10:18:15 +0300

    update vscode project settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 /// <reference path="../types.d.ts" />
+import { Theme } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
-import '@radix-ui/themes/styles.css';
-import { Theme } from '@radix-ui/themes';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Theme>
