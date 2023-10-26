981c59879138f6f35db8886407122e70a259ef3f
Irene Mutua <mutuamirene@gmail.com>
2023-10-26 10:22:09 +0300

    add progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -29,35 +29,44 @@ export default function App() {
   }
 
   return (
-    <Container size="4" style={{ marginTop: '20px', backgroundColor: 'greenyellow', height: '100vh' }}>
-      <Flex direction="column" gap="8" align="center">
-        <Heading as="h1" size="9">
-          Pomodoro
-        </Heading>
+    <>
+      <div>
+        {
+          <progress id="timers" max="100" value="50">
+            50%
+          </progress>
+        }
+      </div>
+      <Container size="4" style={{ marginTop: '20px', backgroundColor: 'greenyellow', height: '100vh' }}>
+        <Flex direction="column" gap="8" align="center">
+          <Heading as="h1" size="9">
+            Pomodoro
+          </Heading>
 
-        <Tabs.Root value={mode} onValueChange={updateMode} style={{ backgroundColor: 'red' }}>
-          <Tabs.List style={{ fontSize: '30px' }}>
-            <Tabs.Trigger value="pomodoro">Pomodoro</Tabs.Trigger>
-            <Tabs.Trigger value="short-break">Short break</Tabs.Trigger>
-            <Tabs.Trigger value="long-break">Long break</Tabs.Trigger>
-          </Tabs.List>
+          <Tabs.Root value={mode} onValueChange={updateMode} style={{ backgroundColor: 'red' }}>
+            <Tabs.List style={{ fontSize: '30px' }}>
+              <Tabs.Trigger value="pomodoro">Pomodoro</Tabs.Trigger>
+              <Tabs.Trigger value="short-break">Short break</Tabs.Trigger>
+              <Tabs.Trigger value="long-break">Long break</Tabs.Trigger>
+            </Tabs.List>
 
-          <Box px="4" pt="3" pb="2">
-            <Tabs.Content value="pomodoro">
-              <Timer initialTime={INITIAL_TIME_IN_SECONDS.pomodoro} switchMode={switchMode} />
-            </Tabs.Content>
+            <Box px="4" pt="3" pb="2">
+              <Tabs.Content value="pomodoro">
+                <Timer initialTime={INITIAL_TIME_IN_SECONDS.pomodoro} switchMode={switchMode} />
+              </Tabs.Content>
 
-            <Tabs.Content value="short-break">
-              <Timer initialTime={INITIAL_TIME_IN_SECONDS.shortBreak} switchMode={switchMode} />
-            </Tabs.Content>
+              <Tabs.Content value="short-break">
+                <Timer initialTime={INITIAL_TIME_IN_SECONDS.shortBreak} switchMode={switchMode} />
+              </Tabs.Content>
 
-            <Tabs.Content value="long-break">
-              <Timer initialTime={INITIAL_TIME_IN_SECONDS.longBreak} switchMode={switchMode} />
-            </Tabs.Content>
-          </Box>
-        </Tabs.Root>
-      </Flex>
-      <footer className="footer">{mode === 'pomodoro' ? 'Time to focus' : 'Time for break'}</footer>
-    </Container>
+              <Tabs.Content value="long-break">
+                <Timer initialTime={INITIAL_TIME_IN_SECONDS.longBreak} switchMode={switchMode} />
+              </Tabs.Content>
+            </Box>
+          </Tabs.Root>
+        </Flex>
+        <footer className="footer">{mode === 'pomodoro' ? 'Time to focus' : 'Time for break'}</footer>
+      </Container>
+    </>
   );
 }
