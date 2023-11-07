426c6f30295465426db25868962ccf6fa3dbd3ac
Irene Mutua <mutuamirene@gmail.com>
2023-11-07 11:00:19 +0300

    implement progress bar

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -5,13 +5,13 @@ const ProgressDemo = () => {
   const [progress, setProgress] = React.useState(13);
 
   React.useEffect(() => {
-    const timer = setTimeout(() => setProgress(66), 500);
+    const timer = setTimeout(() => setProgress(80), 500);
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <Progress.Root className="ProgressRoot" value={progress}>
-      <Progress.Indicator className="ProgressIndicator" style={{ transform: `translateX(-${100 - progress}%)` }} />
+      <Progress.Indicator className="ProgressIndicator" style={{ transform: `translateX(${100 - progress}%)` }} />
     </Progress.Root>
   );
 };
