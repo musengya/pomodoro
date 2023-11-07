f01db6b44ada08d70ff38b2987d85bf5183770e2
Irene Mutua <mutuamirene@gmail.com>
2023-11-07 10:37:09 +0300

    implement progress bar

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -10,19 +10,8 @@ const ProgressDemo = () => {
   }, []);
 
   return (
-    <Progress.Root
-      className="relative overflow-hidden bg-blackA6 rounded-full w-[300px] h-[25px]"
-      style={{
-        // Fix overflow clipping in Safari
-        // https://gist.github.com/domske/b66047671c780a238b51c51ffde8d3a0
-        transform: 'translateZ(0)',
-      }}
-      value={progress}
-    >
-      <Progress.Indicator
-        className="bg-white w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
-        style={{ transform: `translateX(-${100 - progress}%)` }}
-      />
+    <Progress.Root className="ProgressRoot" value={progress}>
+      <Progress.Indicator className="ProgressIndicator" style={{ transform: `translateX(-${100 - progress}%)` }} />
     </Progress.Root>
   );
 };
